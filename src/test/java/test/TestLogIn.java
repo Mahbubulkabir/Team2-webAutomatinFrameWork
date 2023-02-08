5b119c133eeb092c9378c24fb48138bf6dc8eb93
Mahbubulkabir <munnagajaghanta@Gmail.com>
2023-02-08 04:44:43 -0500

    second Hybrid framework

--- src/test/java/test/TestLogIn.java
+++ src/test/java/test/TestLogIn.java
@@ -1,2 +1,22 @@
-package test;public class TestLogIn {
+package test;
+
+import base.CommonAPI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+import utility.ReadFromExel;
+
+public class TestLogIn extends CommonAPI {
+
+    Logger LOG = LogManager.getLogger(TestLogIn.class.getName());
+    ReadFromExel read = new ReadFromExel("C:\\Users\\munna\\InteliiJ\\web-automationFinalbootcamp-Mahbubul\\data\\data.xlsx", "test data");
+    String username= read.getCellValueForGivenHeaderAndKey("key","userName");
+    String password= read.getCellValueForGivenHeaderAndKey("key","passWord");
+
+
+    @Test
+    public void methodName(){
+
+
+    }
 }
